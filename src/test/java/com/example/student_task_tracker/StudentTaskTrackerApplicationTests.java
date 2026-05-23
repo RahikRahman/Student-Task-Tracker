745a65e8f900de20840f284bf15a7072a3806ac7
Rahik Rahman <rahik.rahman@students.mq.edu.au>
2026-05-23 15:00:11 +1000

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/student_task_tracker/StudentTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_task_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
